9089daa3623670e9d27c959585da5600390ea705
cassiano-flores <cassiano.flores2424@gmail.com>
2023-09-22 22:01:14 +0000

    Tutorial 2.2: System Calls - Criando uma nova chamada de sistema

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -8,3 +8,8 @@ chmod +x $BASE_DIR/target/etc/init.d/S50hello
 
 cp $BASE_DIR/../programs/hello/helloi686 $BASE_DIR/target/usr/bin
 cp $BASE_DIR/../programs/tp1/server.py $BASE_DIR/target/root
+
+#Compile the syscall_test.c
+BUILDROOT_DIR=$BASE_DIR/..
+COMPILER=$BUILDROOT_DIR/output/host/bin/i686-buildroot-linux-gnu-gcc
+$COMPILER -o $BUILDROOT_DIR/output/target/bin/syscall_test $BUILDROOT_DIR/syscall_test.c
